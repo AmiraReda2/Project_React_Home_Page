21630f07d2cffe79c005ea708d17b67ac3e3d7cb
AmiraReda2 <amiraredayousef@gmail.com>
2023-09-13 02:29:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import About from './Component/About/About';
+import Contact from './Component/Contact/Contact';
+import Protfolio from './Component/Protfolio/Protfolio';
+import Home from './Component/Home/Home';
+import Layout from './Component/Layout/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+const router= createBrowserRouter([
+ 
+  
+  
+  {path:'', element:<Layout/>, children:[
+    {index: true, element:<Home/>},
+    {path: 'home', element:<Home/>},
+    {path:'about', element:<About/>},
+    {path:'protfolio', element:<Protfolio/>},
+    {path:'contact', element:<Contact/>},
+
+    {path:'*', element: <div className='bg-success d-flex vh-100 justify-content-center align-items-center'>
+       <h2 className='text-white fs-2'>Not Found The Page</h2>
+    </div>}
+
+  ]},
+
+
+])
+  
+
+export default class App extends Component{
+
+
+  render(){
+    return<>
+    <RouterProvider  router={ router } />
+    </>
+    
+  }
+ }
